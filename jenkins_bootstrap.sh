34208be3ea22bd0f59d733200c94ff439440a5c8
Sam Gleske <sam.mxracer@gmail.com>
2018-05-24 21:46:51 -0700

    Configure Jenkinsfile

--- jenkins_bootstrap.sh
+++ jenkins_bootstrap.sh
@@ -5,3 +5,9 @@ create_view --view-name 'Status Overview' --xml-data ./configs/view_status_overv
 create_view --view-name 'Maintenance' --xml-data ./configs/view_maintenance.xml
 create_view --view-name 'Build Pipelines' --xml-data ./configs/view_build_pipelines.xml
 create_job --job-name '_job_generator' --xml-data ./configs/job_generator.xml
+#disable job dsl script security
+jenkins_console -s "${SCRIPT_LIBRARY_PATH}/configure-job-dsl-security.groovy"
+#only enable JNLP4 agent protocol
+jenkins_console -s "${SCRIPT_LIBRARY_PATH}/configure-jnlp-agent-protocols.groovy"
+#global Jenkinsfile
+jenkins_console -s "./scripts/configure-global-jenkinsfile-gimp.groovy"
