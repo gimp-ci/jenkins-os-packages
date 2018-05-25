4beb7e8a08f31559ed2e9ae6666236910c43ba23
Sam Gleske <sam.mxracer@gmail.com>
2018-05-24 21:19:36 -0700

    Create or update views and seed job

--- jenkins_bootstrap.sh
+++ jenkins_bootstrap.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 source jenkins-bootstrap-shared/jenkins_bootstrap.sh
+
+create_view --view-name 'Status Overview' --xml-data ./configs/view_status_overview.xml
+create_view --view-name 'Maintenance' --xml-data ./configs/view_maintenance.xml
+create_view --view-name 'Build Pipelines' --xml-data ./configs/view_build_pipelines.xml
+create_job --job-name '_job_generator' --xml-data ./configs/job_generator.xml
